020d5d83ef145cd381fd6ec8e021cfd62b4a0c5b
xfl03 <xfl03@hotmail.com>
2015-07-15 10:35:45 +0800

    HD Skin Support Signed-off-by: xfl03 <xfl03@hotmail.com>

--- js/2dskin.js
+++ js/2dskin.js
@@ -20,187 +20,214 @@
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the overlays transparent if they have no transparent pixels (how Minecraft does it)
 function FixOverlay(context) {
-	FixHead2(context);
-	FixBody2(context);
-	FixRightArm2(context);
-	FixLeftArm2(context);
-	FixRightLeg2(context);
-	FixLeftLeg2(context);
+	FixOverlay(context,1);
+}
+function FixOverlay(context,radio) {
+	FixHead2(context,radio);
+	FixBody2(context,radio);
+	FixRightArm2(context,radio);
+	FixLeftArm2(context,radio);
+	FixRightLeg2(context,radio);
+	FixLeftLeg2(context,radio);
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the head overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixHead2(context) {
+	FixHead2(context,1);
+}
+function FixHead2(context,radio) {
 	// Front
-	if(HasTransparency(context, 40, 8, 8, 8)) return;
+	if(HasTransparency(context, 40, 8*radio, 8*radio, 8*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 40, 0, 8, 8)) return;
-	if(HasTransparency(context, 48, 0, 8, 8)) return;
-	if(HasTransparency(context, 32, 8, 8, 8)) return;
-	if(HasTransparency(context, 48, 8, 8, 8)) return;
-	if(HasTransparency(context, 56, 8, 8, 8)) return;
+	if(HasTransparency(context, 40, 0      , 8*radio, 8*radio)) return;
+	if(HasTransparency(context, 48, 0      , 8*radio, 8*radio)) return;
+	if(HasTransparency(context, 32*radio, 8*radio, 8*radio, 8*radio)) return;
+	if(HasTransparency(context, 48, 8*radio, 8*radio, 8*radio)) return;
+	if(HasTransparency(context, 56, 8*radio, 8*radio, 8*radio)) return;
 	
 	// Didn't have transparency, clearing the head overlay area.
-	context.clearRect(40, 0, 8, 8);
-	context.clearRect(48, 0, 8, 8);
-	context.clearRect(32, 8, 8, 8);
-	context.clearRect(40, 8, 8, 8);
-	context.clearRect(48, 8, 8, 8);
-	context.clearRect(56, 8, 8, 8);
+	context.clearRect(40*radio, 0      , 8*radio, 8*radio);
+	context.clearRect(48*radio, 0      , 8*radio, 8*radio);
+	context.clearRect(32*radio, 8*radio, 8*radio, 8*radio);
+	context.clearRect(40*radio, 8*radio, 8*radio, 8*radio);
+	context.clearRect(48*radio, 8*radio, 8*radio, 8*radio);
+	context.clearRect(56*radio, 8*radio, 8*radio, 8*radio);
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the body overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixBody2(context) {
+	FixBody2(context,1);
+}
+function FixBody2(context,radio) {
 	// Front
-	if(HasTransparency(context, 20, 36, 8, 12)) return;
+	if(HasTransparency(context, 20*radio, 36*radio, 8*radio, 12*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 20, 32, 8, 4)) return;
-	if(HasTransparency(context, 28, 32, 8, 4)) return;
-	if(HasTransparency(context, 16, 36, 4, 12)) return;
-	if(HasTransparency(context, 28, 36, 4, 12)) return;
-	if(HasTransparency(context, 32, 36, 8, 12)) return;
+	if(HasTransparency(context, 20*radio, 32*radio, 8*radio,  4*radio)) return;
+	if(HasTransparency(context, 28*radio, 32*radio, 8*radio,  4*radio)) return;
+	if(HasTransparency(context, 16*radio, 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 28*radio, 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 32*radio, 36*radio, 8*radio, 12*radio)) return;
 	
 	// Didn't have transparency, clearing the body overlay area.
-	context.clearRect(20, 32, 8, 4);
-	context.clearRect(28, 32, 8, 4);
-	context.clearRect(16, 36, 4, 12);
-	context.clearRect(20, 36, 8, 12);
-	context.clearRect(28, 36, 4, 12);
-	context.clearRect(32, 36, 8, 12);
+	context.clearRect(20*radio, 32*radio, 8*radio,  4*radio);
+	context.clearRect(28*radio, 32*radio, 8*radio,  4*radio);
+	context.clearRect(16*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(20*radio, 36*radio, 8*radio, 12*radio);
+	context.clearRect(28*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(32*radio, 36*radio, 8*radio, 12*radio);
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the right arm overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixRightArm2(context) {
+	FixRightArm2(context,1);
+}
+function FixRightArm2(context,radio) {
 	// Front
-	if(HasTransparency(context, 44, 36, 4, 12)) return;
+	if(HasTransparency(context, 44*radio, 36*radio, 4*radio, 12*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 44, 32, 4, 4)) return;
-	if(HasTransparency(context, 48, 32, 4, 4)) return;
-	if(HasTransparency(context, 40, 36, 4, 12)) return;
-	if(HasTransparency(context, 48, 36, 4, 12)) return;
-	if(HasTransparency(context, 52, 36, 4, 12)) return;
+	if(HasTransparency(context, 44*radio, 32*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context, 48*radio, 32*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context, 40*radio, 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 48*radio, 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 52*radio, 36*radio, 4*radio, 12*radio)) return;
 	
 	// Didn't have transparency, clearing the right arm overlay area.
-	context.clearRect(44, 32, 4, 4);
-	context.clearRect(48, 32, 4, 4);
-	context.clearRect(40, 36, 4, 12);
-	context.clearRect(44, 36, 4, 12);
-	context.clearRect(48, 36, 4, 12);
-	context.clearRect(52, 36, 4, 12);
+	context.clearRect(44*radio, 32*radio, 4*radio,  4*radio);
+	context.clearRect(48*radio, 32*radio, 4*radio,  4*radio);
+	context.clearRect(40*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(44*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(48*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(52*radio, 36*radio, 4*radio, 12*radio);
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the left arm overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixLeftArm2(context) {
+	FixLeftArm2(context,1);
+}
+function FixLeftArm2(context,radio) {
 	// Front
-	if(HasTransparency(context, 52, 52, 4, 12)) return;
+	if(HasTransparency(context, 52*radio, 52*radio, 4*radio, 12*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 52, 48, 4, 4)) return;
-	if(HasTransparency(context, 56, 48, 4, 4)) return;
-	if(HasTransparency(context, 48, 52, 4, 12)) return;
-	if(HasTransparency(context, 56, 52, 4, 12)) return;
-	if(HasTransparency(context, 60, 52, 4, 12)) return;
+	if(HasTransparency(context, 52*radio, 48*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context, 56*radio, 48*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context, 48*radio, 52*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 56*radio, 52*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 60*radio, 52*radio, 4*radio, 12*radio)) return;
 	
 	// Didn't have transparency, clearing the left arm overlay area.
-	context.clearRect(52, 48, 4, 4);
-	context.clearRect(56, 48, 4, 4);
-	context.clearRect(48, 52, 4, 12);
-	context.clearRect(52, 52, 4, 12);
-	context.clearRect(56, 52, 4, 12);
-	context.clearRect(60, 52, 4, 12);
+	context.clearRect(52*radio, 48*radio, 4*radio,  4*radio);
+	context.clearRect(56*radio, 48*radio, 4*radio,  4*radio);
+	context.clearRect(48*radio, 52*radio, 4*radio, 12*radio);
+	context.clearRect(52*radio, 52*radio, 4*radio, 12*radio);
+	context.clearRect(56*radio, 52*radio, 4*radio, 12*radio);
+	context.clearRect(60*radio, 52*radio, 4*radio, 12*radio);
 }
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the right overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixRightLeg2(context) {
+	FixRightLeg2(context,1);
+}
+function FixRightLeg2(context,radio) {
 	// Front
-	if(HasTransparency(context, 4, 36, 4, 12)) return;
+	if(HasTransparency(context,  4*radio, 36*radio, 4*radio, 12*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 4, 32, 4, 4)) return;
-	if(HasTransparency(context, 8, 32, 4, 4)) return;
-	if(HasTransparency(context, 0, 36, 4, 12)) return;
-	if(HasTransparency(context, 8, 36, 4, 12)) return;
-	if(HasTransparency(context, 12, 36, 4, 12)) return;
+	if(HasTransparency(context,  4*radio, 32*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context,  8*radio, 32*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context,  0      , 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context,  8*radio, 36*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 12*radio, 36*radio, 4*radio, 12*radio)) return;
 	
 	// Didn't have transparency, clearing the right leg overlay area.
-	context.clearRect(4, 32, 4, 4);
-	context.clearRect(8, 32, 4, 4);
-	context.clearRect(0, 36, 4, 12);
-	context.clearRect(4, 36, 4, 12);
-	context.clearRect(8, 36, 4, 12);
-	context.clearRect(12, 36, 4, 12);
+	context.clearRect( 4*radio, 32*radio, 4*radio,  4*radio);
+	context.clearRect( 8*radio, 32*radio, 4*radio,  4*radio);
+	context.clearRect( 0      , 36*radio, 4*radio, 12*radio);
+	context.clearRect( 4*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect( 8*radio, 36*radio, 4*radio, 12*radio);
+	context.clearRect(12*radio, 36*radio, 4*radio, 12*radio);
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the left overlay transparent if it is has no transparent pixels (how Minecraft does it)
 function FixLeftLeg2(context) {
+	FixLeftLeg2(context,1);
+}
+function FixLeftLeg2(context,radio) {
 	// Front
-	if(HasTransparency(context, 4, 52, 4, 12)) return;
+	if(HasTransparency(context,  4*radio, 52*radio, 4*radio, 12*radio)) return;
 	
 	// Top, Bottom, Right, Left, Back
-	if(HasTransparency(context, 4, 48, 4, 4)) return;
-	if(HasTransparency(context, 8, 48, 4, 4)) return;
-	if(HasTransparency(context, 0, 52, 4, 12)) return;
-	if(HasTransparency(context, 8, 52, 4, 12)) return;
-	if(HasTransparency(context, 12, 52, 4, 12)) return;
+	if(HasTransparency(context,  4*radio, 48*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context,  8*radio, 48*radio, 4*radio,  4*radio)) return;
+	if(HasTransparency(context,  0      , 52*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context,  8*radio, 52*radio, 4*radio, 12*radio)) return;
+	if(HasTransparency(context, 12*radio, 52*radio, 4*radio, 12*radio)) return;
 	
 	// Didn't have transparency, clearing the left leg overlay area.
-	context.clearRect(4, 48, 4, 4);
-	context.clearRect(8, 48, 4, 4);
-	context.clearRect(0, 52, 4, 12);
-	context.clearRect(4, 52, 4, 12);
-	context.clearRect(8, 52, 4, 12);
-	context.clearRect(12, 52, 4, 12);
+	context.clearRect( 4*radio, 48*radio, 4*radio,  4*radio);
+	context.clearRect( 8*radio, 48*radio, 4*radio,  4*radio);
+	context.clearRect( 0      , 52*radio, 4*radio, 12*radio);
+	context.clearRect( 4*radio, 52*radio, 4*radio, 12*radio);
+	context.clearRect( 8*radio, 52*radio, 4*radio, 12*radio);
+	context.clearRect(12*radio, 52*radio, 4*radio, 12*radio);
 }
 
 // Expects a canvas with a 64x32 Minecraft skin drawn in the very top left corner (0,0)
 // Your canvas should be 64x64 in size to show the skin parts that were converted
 function Convert6432To6464(context) {
+	Convert6432To6464(context,1);
+}
+function Convert6432To6464(context,radio) {
 	// Convert old format to new format
-	Copy(context, 4, 16, 4, 4, 20, 48, true);	// Top Leg
-	Copy(context, 8, 16, 4, 4, 24, 48, true);	// Bottom Leg
-	Copy(context, 0, 20, 4, 12, 24, 52, true);	// Outer Leg
-	Copy(context, 4, 20, 4, 12, 20, 52, true);	// Front Leg
-	Copy(context, 8, 20, 4, 12, 16, 52, true);	// Inner Leg
-	Copy(context, 12, 20, 4, 12, 28, 52, true);	// Back Leg
+	Copy(context,  4*radio, 16*radio, 4*radio,  4*radio, 20*radio, 48*radio, true);	// Top Leg
+	Copy(context,  8*radio, 16*radio, 4*radio,  4*radio, 24*radio, 48*radio, true);	// Bottom Leg
+	Copy(context,  0      , 20*radio, 4*radio, 12*radio, 24*radio, 52*radio, true);	// Outer Leg
+	Copy(context,  4*radio, 20*radio, 4*radio, 12*radio, 20*radio, 52*radio, true);	// Front Leg
+	Copy(context,  8*radio, 20*radio, 4*radio, 12*radio, 16*radio, 52*radio, true);	// Inner Leg
+	Copy(context, 12*radio, 20*radio, 4*radio, 12*radio, 28*radio, 52*radio, true);	// Back Leg
 	
-	Copy(context, 44, 16, 4, 4, 36, 48, true);	// Top Arm
-	Copy(context, 48, 16, 4, 4, 40, 48, true);	// Bottom Arm
-	Copy(context, 40, 20, 4, 12, 40, 52, true);	// Outer Arm
-	Copy(context, 44, 20, 4, 12, 36, 52, true);	// Front Arm
-	Copy(context, 48, 20, 4, 12, 32, 52, true);	// Inner Arm
-	Copy(context, 52, 20, 4, 12, 44, 52, true);	// Back Arm
+	Copy(context, 44*radio, 16*radio, 4*radio,  4*radio, 36*radio, 48*radio, true);	// Top Arm
+	Copy(context, 48*radio, 16*radio, 4*radio,  4*radio, 40*radio, 48*radio, true);	// Bottom Arm
+	Copy(context, 40*radio, 20*radio, 4*radio, 12*radio, 40*radio, 52*radio, true);	// Outer Arm
+	Copy(context, 44*radio, 20*radio, 4*radio, 12*radio, 36*radio, 52*radio, true);	// Front Arm
+	Copy(context, 48*radio, 20*radio, 4*radio, 12*radio, 32*radio, 52*radio, true);	// Inner Arm
+	Copy(context, 52*radio, 20*radio, 4*radio, 12*radio, 44*radio, 52*radio, true);	// Back Arm
 }
 
 // Expects a canvas with a Minecraft skin drawn in the very top left corner (0,0)
 // Makes the non-visible parts of the Minecraft skin transparent
 function FixNonVisible(context) {
+	FixNonVisible(context,1);
+}
+function FixNonVisible(context,radio) {
 	// 64x32 and 64x64 skin parts
-	context.clearRect(0, 0, 8, 8);
-	context.clearRect(24, 0, 16, 8);
-	context.clearRect(56, 0, 8, 8);
-	context.clearRect(0, 16, 4, 4);
-	context.clearRect(12, 16, 8, 4);
-	context.clearRect(36, 16, 8, 4);
-	context.clearRect(52, 16, 4, 4);
-	context.clearRect(56, 16, 8, 32);
+	context.clearRect( 0      ,  0      ,  8*radio,  8*radio);
+	context.clearRect(24*radio,  0      , 16*radio,  8*radio);
+	context.clearRect(56*radio,  0      ,  8*radio,  8*radio);
+	context.clearRect( 0      , 16*radio,  4*radio,  4*radio);
+	context.clearRect(12*radio, 16*radio,  8*radio,  4*radio);
+	context.clearRect(36*radio, 16*radio,  8*radio,  4*radio);
+	context.clearRect(52*radio, 16*radio,  4*radio,  4*radio);
+	context.clearRect(56*radio, 16*radio,  8*radio, 32*radio);
 	
 	// 64x64 skin parts
-	context.clearRect(0, 32, 4, 4);
-	context.clearRect(12, 32, 8, 4);
-	context.clearRect(36, 32, 8, 4);
-	context.clearRect(52, 32, 4, 4);
-	context.clearRect(0, 48, 4, 4);
-	context.clearRect(12, 48, 8, 4);
-	context.clearRect(28, 48, 8, 4);
-	context.clearRect(44, 48, 8, 4);
-	context.clearRect(60, 48, 8, 4);
+	context.clearRect( 0      , 32*radio, 4*radio, 4*radio);
+	context.clearRect(12*radio, 32*radio, 8*radio, 4*radio);
+	context.clearRect(36*radio, 32*radio, 8*radio, 4*radio);
+	context.clearRect(52*radio, 32*radio, 4*radio, 4*radio);
+	context.clearRect( 0      , 48*radio, 4*radio, 4*radio);
+	context.clearRect(12*radio, 48*radio, 8*radio, 4*radio);
+	context.clearRect(28*radio, 48*radio, 8*radio, 4*radio);
+	context.clearRect(44*radio, 48*radio, 8*radio, 4*radio);
+	context.clearRect(60*radio, 48*radio, 8*radio, 4*radio);
 }
 
 // Checks if the given part of the canvas contains a pixel with 0 alpha value (transparent)
